a6a2dadb8a54f0751760d27606bbc924719ff027
DESKTOP-EAO30GT\86136 <1435554190@qq.com>
2021-11-08 23:27:36 +0800

    基础

---
+++ hello-vue3/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App);
+
+app.component('todo-item', {
+    props: ['todo'],
+    template: `<li>{{ todo.text }}</li>`,
+})
+
+app.mount('#app');
